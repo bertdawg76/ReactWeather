a150eea848c7ba7007a08d50c3555d684f15e9ab
bertdawg76 <bertcoleman76@gmail.com>
2016-07-21 15:34:47 -0600

    styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,23 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-/*var About = React.createClass({
+/*About = React.createClass({
   render: function(){
     return (
-      <h3>About Component</h3>
-    );
+      <div>
+        <h3>About This Site</h3>
+        <p>This Site makes an API call to the Open Weather Map API and displays the temperature.</p>
+        <p>Below are links to the Open Weather Map site and to my github repo</p>
+        <ol>
+          <li>
+            <Link to='https://github.com/bertdawg76/ReactWeather'>Github Link</Link>
+          </li>
+          <li>
+            <Link to='http://openweathermap.org/api'>Open Weather Map API link</Link>
+          </li>
+        </ol>
+      </div>
+    )
   }
 });*/
 
@@ -12,10 +25,19 @@ var About =  (props) => {
 
   return (
     <div>
-      <h3>About Component</h3>
-      <p>Welcome to the About page</p>
+      <h3 className="text-center">About This Site</h3>
+      <p>This Site uses React to  makes an API call to the Open Weather Map API and displays the temperature.</p>
+      <p>Below are links to the Open Weather Map site and to my github repo</p>
+      <ul>
+        <li>
+          <a href="https://github.com/bertdawg76/ReactWeather" target="_blank">Github Link</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org/api" target="_blank">Open Weather Map API link</a>
+        </li>
+      </ul>
     </div>
   )
-}
+};
 
 module.exports = About;
