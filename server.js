58b2b3eb746f54b548f09a660b3afa8812c45ef2
bertdawg76 <bertcoleman76@gmail.com>
2016-07-21 08:56:07 -0600

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+
+});
 
 app.use(express.static('public'));
 
-app.listen(8800, function (){
-  console.log('Server running on port 8800');
+
+app.listen(PORT, function (){
+  console.log('Server running on port ' + PORT);
 });
